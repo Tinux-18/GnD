089caf66c69730691baeda35d5e71e55a29ce60b
Constantin Rig <rigucatalin@gmail.com>
2022-03-09 16:20:28 +0100

    Part 1 set-up

--- client/src/start.js
+++ client/src/start.js
@@ -1,7 +1,9 @@
 import ReactDOM from "react-dom";
+import Welcome from "./welcome";
 
-ReactDOM.render(<HelloWorld />, document.querySelector("main"));
+fetch("/user/id.json");
+ReactDOM.render(<Welcome />, document.querySelector("main"));
 
-function HelloWorld() {
-    return <div>Hello, World!</div>;
-}
+// function HelloWorld() {
+//     return <div>Hello, World & Fuck you!</div>;
+// }
--- server/server.js
+++ server/server.js
@@ -2,15 +2,30 @@ const express = require("express");
 const app = express();
 const compression = require("compression");
 const path = require("path");
+const logger = require("morgan");
+const { hash, compare } = require("./bc");
+// const db = require("./database/db");
 
 app.use(compression());
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
+
+app.use(function (req, res, next) {
+    res.setHeader("x-frame-options", "deny");
+    next();
+}); // middleware to prevent your site from being used in clickjacking
+
+app.get("/user/id.json", function (req, res) {
+    res.status("200");
+    res.json({ userId: "cookie session shit" });
+});
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "..", "client", "index.html"));
 });
 
 app.listen(process.env.PORT || 3001, function () {
-    console.log("I'm listening.");
+    console.log("SLP listening \nhttp://localhost:3000");
 });
